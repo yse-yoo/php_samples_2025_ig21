4eb29d6aaa0acd8688ce2e0edc354da5ce42b854
yse-yoo <youhei.yoshikawa@yse-c.net>
2025-04-08 11:07:27 +0900

    update

---
+++ basic/env.php
@@ -0,0 +1,8 @@
+<?php
+const DB_CONNECTION = 'mysql';
+const DB_HOST = 'localhost';
+const DB_PORT = '3306';
+const DB_USERNAME = 'root';
+const DB_PASSWORD = '';
+const DB_DATABASE = 'php_sns';
+const DB_CHARSET = 'utf8mb4';
