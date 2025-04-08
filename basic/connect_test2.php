39e9fff46793994502770c6b467efc4a159b6a1b
yse-yoo <youhei.yoshikawa@yse-c.net>
2025-04-08 11:48:47 +0900

    update

--- basic/connect_test2.php
+++ basic/connect_test2.php
@@ -6,3 +6,19 @@ require_once __DIR__ . '/lib/Database.php';
 
 // Database connection
 $db = Database::getInstance();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
