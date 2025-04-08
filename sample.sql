719bd2ecef7629eea49ec1314b397d6b080472c7
yse-yoo <youhei.yoshikawa@yse-c.net>
2025-04-08 10:46:02 +0900

    updaet

--- sample.sql
+++ sample.sql
@@ -1,3 +1,8 @@
 SELECT * FROM users WHERE id = 5;
 
 SELECT * FROM tweets WHERE user_id = 1;
+
+-- tweets.id = 1 のレコードの users.display_name を取得
+SELECT users.display_name FROM users 
+JOIN tweets ON users.id = tweets.user_id 
+WHERE tweets.id = 1;
